8629e0d569bde8755bd9972d261ff07768f838c1
Sergey Sipatrin <ssipatrin@webinerds.com>
2019-03-14 17:36:29 +0200

    Book entity

---
+++ src/entity/book.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinTable } from 'typeorm';
+import { Length, IsDate } from "class-validator";
+import { User } from './user';
+
+@Entity()
+export class Book {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        length: 80
+    })
+    @Length(1,80)
+    name: string;
+
+    @Column({
+        type: 'text'
+    })
+    @Length(1,100)
+    description: string;
+
+    @IsDate()
+    date: Date;
+
+    @ManyToOne(type => User, user => user.books)
+    @JoinTable({ name: 'user_id'})
+    user: User;
+}
